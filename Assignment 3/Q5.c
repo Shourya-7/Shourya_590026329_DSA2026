5e120b5617867adaed6caf744b0bd00e506f30a1
Shourya-7 <shourya3477@gmail.com>
2026-04-08 16:43:34 +0530

    Update Q5.c

--- Assignment 3/Q5.c
+++ Assignment 3/Q5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 char stack[100];
 int top = -1;
@@ -7,32 +8,31 @@ int top = -1;
 void push(char c) { stack[++top] = c; }
 char pop() { return stack[top--]; }
 
-int precedence(char c) {
+int prec(char c) {
     if (c == '^') return 3;
     if (c == '*' || c == '/') return 2;
     if (c == '+' || c == '-') return 1;
     return 0;
 }
 
-void infixToPostfix(char* infix) {
-    for (int i = 0; infix[i] != '\0'; i++) {
+int main() {
+    char infix[100];
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    printf("Postfix expression: ");
+    for (int i = 0; i < strlen(infix); i++) {
         if (isalnum(infix[i])) printf("%c", infix[i]);
         else if (infix[i] == '(') push('(');
         else if (infix[i] == ')') {
-            while (top != -1 && stack[top] != '(') printf("%c", pop());
+            while (stack[top] != '(') printf("%c", pop());
             pop();
         } else {
-            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) printf("%c", pop());
+            while (top != -1 && prec(stack[top]) >= prec(infix[i])) printf("%c", pop());
             push(infix[i]);
         }
     }
     while (top != -1) printf("%c", pop());
-}
-
-int main() {
-    char infix[] = "A+B*(C^D-E)^(F+G*H)-I";
-    printf("Postfix: ");
-    infixToPostfix(infix);
     printf("\n");
     return 0;
 }
