5b837c380056435136763c2611fcff2aaf4ff313
Shourya-7 <shourya3477@gmail.com>
2026-04-08 16:44:48 +0530

    Update Q6.c

--- Assignment 3/Q6.c
+++ Assignment 3/Q6.c
@@ -8,18 +8,19 @@ void push(int n) { stack[++top] = n; }
 int pop() { return stack[top--]; }
 
 int main() {
-    char exp[] = "23*54*+9-";
+    char exp[100];
+    printf("Enter postfix expression (e.g., 23*54*+9-): ");
+    scanf("%s", exp);
+
     for (int i = 0; exp[i] != '\0'; i++) {
         if (isdigit(exp[i])) push(exp[i] - '0');
         else {
-            int val2 = pop();
-            int val1 = pop();
-            switch (exp[i]) {
-                case '+': push(val1 + val2); break;
-                case '-': push(val1 - val2); break;
-                case '*': push(val1 * val2); break;
-                case '/': push(val1 / val2); break;
-            }
+            int v2 = pop();
+            int v1 = pop();
+            if (exp[i] == '+') push(v1 + v2);
+            else if (exp[i] == '-') push(v1 - v2);
+            else if (exp[i] == '*') push(v1 * v2);
+            else if (exp[i] == '/') push(v1 / v2);
         }
     }
     printf("Result = %d\n", pop());
