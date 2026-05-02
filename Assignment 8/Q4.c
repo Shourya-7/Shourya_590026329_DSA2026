dfd18155d5dac8e83060c3ac465cc96e2a6c3f95
Shourya-7 <shourya3477@gmail.com>
2026-05-02 18:16:48 +0530

    Create Q4.c

---
+++ Assignment 8/Q4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i=l, j=m+1, k=0;
+    int temp[100];
+
+    while(i<=m && j<=r) {
+        if(arr[i] < arr[j]) temp[k++] = arr[i++];
+        else temp[k++] = arr[j++];
+    }
+
+    while(i<=m) temp[k++] = arr[i++];
+    while(j<=r) temp[k++] = arr[j++];
+
+    for(i=l,k=0;i<=r;i++,k++)
+        arr[i] = temp[k];
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if(l<r) {
+        int m = (l+r)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+}
+
+int main() {
+    int n, arr[100];
+
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    printf("Enter elements:\n");
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+
+    mergeSort(arr,0,n-1);
+
+    printf("Sorted array: ");
+    for(int i=0;i<n;i++) printf("%d ", arr[i]);
+
+    printf("\nName: %s\n", name);
+
+    return 0;
+}
