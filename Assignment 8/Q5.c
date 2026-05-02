015e4dd0da297717bae00789e76b6ee4ef702e8d
Shourya-7 <shourya3477@gmail.com>
2026-05-02 18:17:03 +0530

    Create Q5.c

---
+++ Assignment 8/Q5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1, temp;
+
+    for(int j=low;j<high;j++) {
+        if(arr[j] < pivot) {
+            i++;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+
+    return i+1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if(low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+
+int main() {
+    int n, arr[100];
+
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    printf("Enter elements:\n");
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+
+    quickSort(arr,0,n-1);
+
+    printf("Sorted array: ");
+    for(int i=0;i<n;i++) printf("%d ", arr[i]);
+
+    printf("\nName: %s\n", name);
+
+    return 0;
+}
